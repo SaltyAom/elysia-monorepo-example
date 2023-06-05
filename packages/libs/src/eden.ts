57c5e006883535cf769b964df2121e2234b82c0b
Verhille <titouan.verhille@epita.fr>
2023-06-05 17:44:19 +0900

    update version

--- packages/libs/src/eden.ts
+++ packages/libs/src/eden.ts
@@ -1,4 +1,4 @@
-import { edenTreaty } from '@elysiajs/eden'
-import type { App } from 'backend'
+import { edenTreaty } from "@elysiajs/eden"
+import type { App } from "backend"
 
-export const api = edenTreaty<App>('http://0.0.0.0:3000/')
+export const api = edenTreaty<App>("http://0.0.0.0:3000/")
